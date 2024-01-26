52c2876b5a8f9e27078d2243fd37328bde2627ce
Mahdinkh7 <mahdinkh7@gmail.com>
2024-01-26 23:44:57 +0330

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:sidebarx/sidebarx.dart';
+
+import 'common/widgets.dart';
+import 'pages/home/home.dart';
+import 'theme/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  final _controller = SidebarXController(selectedIndex: 0, extended: true);
+  final _key = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Builder(
+        builder: (context) {
+          final isSmallScreen = MediaQuery.of(context).size.width < 600;
+          return Scaffold(
+            key: _key,
+            appBar: isSmallScreen
+                ? AppBar(
+                    backgroundColor: canvasColor,
+                    title: Text(getTitleByIndex(_controller.selectedIndex)),
+                    leading: IconButton(
+                      onPressed: () {
+                        _key.currentState?.openDrawer();
+                      },
+                      icon: const Icon(Icons.menu),
+                    ),
+                  )
+                : null,
+            drawer: Sidebar(controller: _controller),
+            body: Row(
+              children: [
+                if (!isSmallScreen) Sidebar(controller: _controller),
+                Expanded(
+                  child: Center(
+                    child: ScreensExample(
+                      controller: _controller,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sidebarx/sidebarx.dart';
+
+import '../../common/widgets.dart';
+import '../../theme/theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,5 +10,147 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+}
+
+class Sidebar extends StatelessWidget {
+  const Sidebar({
+    Key? key,
+    required SidebarXController controller,
+  })  : _controller = controller,
+        super(key: key);
+
+  final SidebarXController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SidebarX(
+        controller: _controller,
+        theme: SidebarXTheme(
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: canvasColor,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          hoverColor: scaffoldBackgroundColor,
+          textStyle: TextStyle(color: Colors.white.withOpacity(0.7)),
+          selectedTextStyle: const TextStyle(color: Colors.white),
+          itemTextPadding: const EdgeInsets.only(left: 30),
+          selectedItemTextPadding: const EdgeInsets.only(left: 30),
+          itemDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: canvasColor),
+          ),
+          selectedItemDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: actionColor.withOpacity(0.37),
+            ),
+            gradient: const LinearGradient(
+              colors: [accentCanvasColor, canvasColor],
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.28),
+                blurRadius: 30,
+              )
+            ],
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.white.withOpacity(0.7),
+            size: 20,
+          ),
+          selectedIconTheme: const IconThemeData(
+            color: Colors.white,
+            size: 20,
+          ),
+        ),
+        extendedTheme: const SidebarXTheme(
+          width: 200,
+          decoration: BoxDecoration(
+            color: canvasColor,
+          ),
+        ),
+        footerDivider: divider,
+        headerBuilder: (context, extended) {
+          return SizedBox(
+            height: 100,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset('assets/profile.jpg')),
+            ),
+          );
+        },
+        items: [
+          SidebarXItem(
+            icon: Icons.home,
+            label: 'Home',
+            onTap: () {
+              debugPrint('Home');
+            },
+          ),
+          const SidebarXItem(
+            icon: Icons.search,
+            label: 'Search',
+          ),
+          const SidebarXItem(
+            icon: Icons.people,
+            label: 'People',
+          ),
+          const SidebarXItem(
+            icon: Icons.favorite,
+            label: 'Favorites',
+          ),
+          const SidebarXItem(
+            iconWidget: FlutterLogo(size: 20),
+            label: 'Flutter',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ScreensExample extends StatelessWidget {
+  const ScreensExample({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final SidebarXController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        final pageTitle = getTitleByIndex(controller.selectedIndex);
+        switch (controller.selectedIndex) {
+          case 0:
+            return ListView.builder(
+              padding: const EdgeInsets.only(top: 10),
+              itemBuilder: (context, index) => Container(
+                height: 100,
+                width: double.infinity,
+                margin: const EdgeInsets.only(bottom: 10, right: 10, left: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Theme.of(context).canvasColor,
+                  boxShadow: const [BoxShadow()],
+                ),
+              ),
+            );
+          default:
+            return Text(
+              pageTitle,
+              style: theme.textTheme.headlineSmall,
+            );
+        }
+      },
+    );
   }
 }
